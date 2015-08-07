fdd27945f2d652cfdfa563bfe1d395f752f98e3e
Eric Brelsford <ebrelsford@gmail.com>
2015-08-07 10:37:35 -0400

    Do things on each repo

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var gith = require('gith').create(9001);
+var spawn = require('child_process').spawn;
 
 gith.ips.push('*');
 gith().on('all', function(payload) {
-    console.log('Post-receive happened!');
+    if (payload.repo === 'curbyourlitter/curbyourlitter-sidewalk' && payload.branch === 'gh-pages') {
+        spawn('bash', ['~/sidewalk/build/pull.sh']);
+    }
+    if (payload.repo === 'curbyourlitter/curbyourlitter-alley' && payload.branch === 'master') {
+        // Nothing yet, but consider deploying
+    }
 });
