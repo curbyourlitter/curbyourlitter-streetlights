99a05d2d1220b2c915bc00a07dbd08b6523b3836
Eric Brelsford <ebrelsford@gmail.com>
2015-08-10 11:43:51 -0400

    Log stdout and stderr

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ gith.ips.push('*');
 gith().on('all', function(payload) {
     console.log('Gith detected activity on ' + payload.repo + '#' + payload.branch);
     if (payload.repo === 'curbyourlitter/curbyourlitter-sidewalk' && payload.branch === 'gh-pages') {
-        spawn('bash', ['~/sidewalk/build/pull.sh']);
+        console.log('Push to curbyourlitter-sidewalk detected, pulling');
+        var pull = spawn('bash', ['~/sidewalk/build/pull.sh']);
+        pull.stdout.on('data', function (data) {
+            console.log(data);
+        });
+        pull.stderr.on('data', function (data) {
+            console.log(data);
+        });
+        pull.on('close', function (code) {
+            console.log('Pull exited with code ' + code);
+        });
     }
     if (payload.repo === 'curbyourlitter/curbyourlitter-alley' && payload.branch === 'master') {
         // Nothing yet, but consider deploying
