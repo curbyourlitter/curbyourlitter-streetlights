68c7cb623b1b7fa9aae937390a5790a32a746a01
Eric Brelsford <ebrelsford@gmail.com>
2015-08-10 11:40:30 -0400

    Log when event is received

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var spawn = require('child_process').spawn;
 
 gith.ips.push('*');
 gith().on('all', function(payload) {
+    console.log('Gith detected activity on ' + payload.repo + '#' + payload.branch);
     if (payload.repo === 'curbyourlitter/curbyourlitter-sidewalk' && payload.branch === 'gh-pages') {
         spawn('bash', ['~/sidewalk/build/pull.sh']);
     }
